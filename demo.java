1f2da98e21753a5859b8aaf8f9e1f2a1fd21d699
Amar Yele <amaryele2018@gmail.com>
2022-12-17 06:27:44 +0530

    Changes made in Demo.java updated m1()

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public class demo{
 	public static void main(String[] args){
 	
 	System.out.println("Demo Java File");
+	
+	System.out.println("this is line modified for the testing the github");
+
 	}
 }
